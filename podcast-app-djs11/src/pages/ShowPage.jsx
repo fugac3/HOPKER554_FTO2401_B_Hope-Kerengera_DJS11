4abbbc2ffa88507f5d14e47f170f7ad6b52b9ebc
Hope <k.hope.ing@gmail.com>
2024-06-20 13:38:47 +0200

    Added List of Seasons to Show Page

--- podcast-app-djs11/src/pages/ShowPage.jsx
+++ podcast-app-djs11/src/pages/ShowPage.jsx
@@ -24,17 +24,31 @@ export default function ShowPage() {
 
 
   return (
-    <div className="card show-page d-flex flex-row gap-4 align-items-center p-3 my-3 w-100"> 
-    <div className="d-flex">
-        <img className='show-image' src={show.image} alt={show.title} />
+    <div className="show-page-wrapper">
+      <div className="card show-page d-flex flex-row gap-4 align-items-center p-3 my-3 w-100">
+        <div className="d-flex">
+          <img className='show-image' src={show.image} alt={show.title} />
+        </div>
+        <div className="d-flex flex-column justify-content-center flex-grow-1">
+          <h2 className='show-title'>{show.title}</h2>
+          <p className='show-date'><span className='fw-bold'>Last updated:</span> {showDate(show.updated)}</p>
+          <p className='show-description'>{show.description}</p>
+          <p className='show-seasons'><span className='fw-bold'>Seasons:</span> {show.seasons.length}</p>
+        </div>
+      </div>
+      <div className="seasons-wrapper">
+        {show.seasons.map(season => (
+          <div key={season.id} className="card season-card d-flex flex-row gap-4 align-items-center p-3 my-3 w-100">
+            <div className="d-flex">
+              <img className='season-image' src={season.image} alt={season.title} />
+            </div>
+            <div className="d-flex flex-column">
+              <h3 className='season-title'>{season.title}</h3>
+              <p className='season-episodes'><span className='fw-bold'>Episodes:</span> {season.episodes.length}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-    <div className="d-flex flex-column justify-content-center flex-grow-1">
-        <h2 className='show-title'>{show.title}</h2>
-        <p className='show-date'><span className='fw-bold'>Last updated:</span> {showDate(show.updated)}</p>
-        <p className='show-description'>{show.description}</p>
-        <p className='show-seasons'><span className='fw-bold'>Seasons:</span> {show.seasons.length}</p>
-    </div>
-</div>
   );
 }
-
